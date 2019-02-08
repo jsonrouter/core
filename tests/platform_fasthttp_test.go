af712d4d1ea763f8a348e34f670a1a52c4869da2
chainetix <chainetix@gmail.com>
2019-02-08 19:49:25 +0000

    add http tests

--- tests/platform_fasthttp_test.go
+++ tests/platform_fasthttp_test.go
@@ -7,14 +7,11 @@ import (
 	//
 	"github.com/go-resty/resty"
 	//
+	"github.com/jsonrouter/validation"
 	"github.com/jsonrouter/core/http"
 	"github.com/jsonrouter/logging/testing"
 	"github.com/jsonrouter/core/openapi/v2"
 	"github.com/jsonrouter/platforms/fasthttp"
-)
-
-const (
-	CONST_TEST_PORT = 999
 )
 
 func TestFastHttp(t *testing.T) {
@@ -31,20 +28,17 @@ func TestFastHttp(t *testing.T) {
 
 	met := root.Config.Metrics
 
-	endpoint := root.Add("/endpoint")
+	endpoint := root.Add("/endpoint").Param(validation.Int(), "x")
 
 	endpoint.GET(
 		func (req http.Request) *http.Status {
 
-			defer func() {
-
-				val := met.Counters["requestCount"].GetValue()
-				if val != 1 {
-					req.Log().Debugf("CORRECT VALUE IS %v", val)
-					t.Fail()
-				}
-
-			}()
+			x := req.Param("x").(int)
+			val := met.Counters["requestCount"].GetValue()
+			if int(val) != x {
+				req.Log().Debugf("CORRECT VALUE IS %v", x)
+				return req.Fail()
+			}
 
 			return nil
 		},
@@ -53,22 +47,19 @@ func TestFastHttp(t *testing.T) {
 	endpoint.POST(
 		func (req http.Request) *http.Status {
 
-			defer func() {
-
-				val := met.Counters["requestCount"].GetValue()
-				if val != 1 {
-					req.Log().Debugf("CORRECT VALUE IS %v", val)
-					t.Fail()
-				}
-
-			}()
+			x := req.Param("x").(int)
+			val := met.Counters["requestCount"].GetValue()
+			if int(val) != x {
+				req.Log().Debugf("CORRECT VALUE IS %v", x)
+				return req.Fail()
+			}
 
 			return nil
 		},
 	)
 
 	go func() {
-		if err := service.Serve(CONST_TEST_PORT); err != nil {
+		if err := service.Serve(CONST_PORT); err != nil {
 			t.Error(err)
 			t.Fail()
 		}
@@ -76,22 +67,27 @@ func TestFastHttp(t *testing.T) {
 
 	time.Sleep(time.Second)
 
-	url := fmt.Sprintf("http://localhost:%d/endpoint", CONST_PORT)
+	for x := 0; x < 10; x+=2 {
 
-	resp, err := resty.R().Get(url)
-	if log.Error(err) || resp.StatusCode() == 500 {
-		log.NewError(resp.String())
-		t.Fail()
-		return
+		url := fmt.Sprintf("http://localhost:%d/endpoint/%d", CONST_PORT, x)
+
+		log.Debug(url)
+
+		resp, err := resty.R().Get(url)
+		if log.Error(err) || resp.StatusCode() == 500 {
+			log.NewError(resp.String())
+			t.Fail()
+			return
+		}
+
+		resp, err = resty.R().Post(url)
+		if log.Error(err) || resp.StatusCode() == 500 {
+			log.NewError(resp.String())
+			t.Fail()
+			return
+		}
+
 	}
-
-	resp, err = resty.R().Post(url)
-	if log.Error(err) || resp.StatusCode() == 500 {
-		log.NewError(resp.String())
-		t.Fail()
-		return
-	}
-
 	time.Sleep(3 * time.Second)
 
 }
